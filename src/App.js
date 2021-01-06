5db41511a0421be00dc62681a6d446c3f5b61290
dongwook <donguk7858@gmail.com>
2021-01-06 12:59:53 +0900

    Plus or Minus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  state={
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count : current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count : current.count - 1 }));
+  };
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
